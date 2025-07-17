cee86be3be66133109ce42cd5ce712014482b51d
Gokdeniz Kaymak <gokdeniz.kaymak@apify.com>
2025-07-17 20:12:50 +0200

    fix: trying to fix the tests

--- nodes/ApifyContentCrawler/__tests__/Apify.node.spec.ts
+++ nodes/ApifyContentCrawler/__tests__/Apify.node.spec.ts
@@ -67,41 +67,42 @@ describe('Apify Node', () => {
 				expect(scope.isDone()).toBe(true);
 			});
 
-			it('should run the run-actor workflow and wait for finish', async () => {
-				const mockRunActor = fixtures.runActorResult();
-				const mockBuild = fixtures.getBuildResult();
-				const mockFinishedRun = fixtures.getSuccessRunResult();
+			// Again if we want to include wait for finish, this should be uncommented.
+			// it('should run the run-actor workflow and wait for finish', async () => {
+			// 	const mockRunActor = fixtures.runActorResult();
+			// 	const mockBuild = fixtures.getBuildResult();
+			// 	const mockFinishedRun = fixtures.getSuccessRunResult();
 
-				const scope = nock('https://api.apify.com')
-					.get(`/v2/acts/${ACTOR_ID}`)
-					.reply(200, fixtures.getActorResult())
-					.get(`/v2/acts/${ACTOR_ID}/builds/default`)
-					.reply(200, mockBuild)
-					.post(`/v2/acts/${ACTOR_ID}/runs`)
-					.query({ waitForFinish: 0, build: mockBuild.data.buildNumber, memory: 1024 })
-					.reply(200, mockRunActor)
-					.get('/v2/actor-runs/Icz6E0IHX0c40yEi7')
-					.reply(200, mockFinishedRun);
+			// 	const scope = nock('https://api.apify.com')
+			// 		.get(`/v2/acts/${ACTOR_ID}`)
+			// 		.reply(200, fixtures.getActorResult())
+			// 		.get(`/v2/acts/${ACTOR_ID}/builds/default`)
+			// 		.reply(200, mockBuild)
+			// 		.post(`/v2/acts/${ACTOR_ID}/runs`)
+			// 		.query({ waitForFinish: 0, build: mockBuild.data.buildNumber, memory: 1024 })
+			// 		.reply(200, mockRunActor)
+			// 		.get('/v2/actor-runs/Icz6E0IHX0c40yEi7')
+			// 		.reply(200, mockFinishedRun);
 
-				const runActorWorkflow = require('./workflows/actors/run-actor-wait-for-finish.workflow.json');
-				const { waitPromise } = await executeWorkflow({
-					credentialsHelper,
-					workflow: runActorWorkflow,
-				});
-				const result = await waitPromise.promise();
+			// 	const runActorWorkflow = require('./workflows/actors/run-actor-wait-for-finish.workflow.json');
+			// 	const { waitPromise } = await executeWorkflow({
+			// 		credentialsHelper,
+			// 		workflow: runActorWorkflow,
+			// 	});
+			// 	const result = await waitPromise.promise();
 
-				const nodeResults = getRunTaskDataByNodeName(result, 'Run actor');
-				expect(nodeResults.length).toBe(1);
-				const [nodeResult] = nodeResults;
-				expect(nodeResult.executionStatus).toBe('success');
+			// 	const nodeResults = getRunTaskDataByNodeName(result, 'Run actor');
+			// 	expect(nodeResults.length).toBe(1);
+			// 	const [nodeResult] = nodeResults;
+			// 	expect(nodeResult.executionStatus).toBe('success');
 
-				const data = getTaskData(nodeResult);
-				// exptect polled terminal run as result
-				expect(data).not.toEqual(mockRunActor.data);
-				expect(data).toEqual(mockFinishedRun.data);
+			// 	const data = getTaskData(nodeResult);
+			// 	// exptect polled terminal run as result
+			// 	expect(data).not.toEqual(mockRunActor.data);
+			// 	expect(data).toEqual(mockFinishedRun.data);
 
-				expect(scope.isDone()).toBe(true);
-			});
+			// 	expect(scope.isDone()).toBe(true);
+			// });
 		});
 	});
 });
